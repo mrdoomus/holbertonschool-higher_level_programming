22a077d720508eb3c4cf4f5ace42c26dd9e97356
mrdoomus <mrdoom.official@gmail.com>
2019-09-09 22:14:30 +0000

    10-check_cycle C script and lists.h header file, checks if linked list has a loop (1) if not (0)

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * check_cycle - Checks if a linked list has a cycle
+ * @list: Pointer to head
+ * Return: 1 if it has a cycle, 0 if it doesn't
+**/
 int check_cycle(listint_t *list)
 {
 	int i = 0;
