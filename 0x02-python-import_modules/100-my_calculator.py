3deb21615d6c30bc1aac74ba647f49a10464be1f
mrdoomus <mrdoom.official@gmail.com>
2019-09-12 21:52:42 +0000

    Fixing this script...

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,15 +9,14 @@ if __name__ == '__main__':
         op = sys.argv[2]
         a = int(sys.argv[1])
         b = int(sys.argv[3])
-        if op != "+" and op != "-" and op != "/" and op != "*":
+        if op == '+':
+            print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
+        elif op == '-':
+            print("{0:d} - {1:d} = {2:d}".format(a, b, sub(a, b)))
+        elif op == '*':
+            print("{0:d} * {1:d} = {2:d}".format(a, b, mul(a, b)))
+        elif op == '/':
+            print("{0:d} / {1:d} = {2:d}".format(a, b, div(a, b)))
+        else:
             print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
-        else:
-            if op == "+":
-                print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
-            if op == "-":
-                print("{0:d} - {1:d} = {2:d}".format(a, b, sub(a, b)))
-            if op == "*":
-                print("{0:d} * {1:d} = {2:d}".format(a, b, mul(a, b)))
-            if op == "/":
-                print("{0:d} / {1:d} = {2:d}".format(a, b, div(a, b)))
