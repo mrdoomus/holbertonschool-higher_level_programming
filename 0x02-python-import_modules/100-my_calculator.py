8091c406eb9308e07cbc5509b408450a1d188701
mrdoomus <mrdoom.official@gmail.com>
2019-09-13 00:02:08 +0000

    I don't know wtf is happening

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,20 +5,20 @@ if __name__ == '__main__':
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
+    op = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    result = 0
+    if op == "+":
+        result = add(a, b)
+    elif op == "-":
+        result = sub(a, b)
+    elif op == "*":
+        result = mul(a, b)
+    elif op == "/":
+        result = div(a, b)
     else:
-        op = sys.argv[2]
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        result = 0
-        if op == '+':
-            result = add(a, b)
-        elif op == '-':
-            result = sub(a, b)
-        elif op == '*':
-            result = mul(a, b)
-        elif op == '/':
-            result = div(a, b)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
-        print("{0:d} {1} {2:d} = {3:d}".format(a, op, b, result))
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    print("{:d} {} {:d} = {:d}".format(a, op, b, result))
