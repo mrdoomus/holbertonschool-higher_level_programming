18c172af187cbc6ab61e991720ffc4bd6c3d3d02
mrdoomus <mrdoom.official@gmail.com>
2019-09-13 04:30:35 +0000

    Imported just argv and exit from sys

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    op = sys.argv[2]
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
+    op = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
     result = 0
     if op == "+":
         result = add(a, b)
@@ -20,5 +20,5 @@ if __name__ == '__main__':
         result = div(a, b)
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
     print("{:d} {} {:d} = {:d}".format(a, op, b, result))
