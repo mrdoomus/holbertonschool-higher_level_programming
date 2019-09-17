1e3463fe00ecba2c0a0d7978669b2309d4c97334
mrdoomus <mrdoom.official@gmail.com>
2019-09-17 19:42:04 +0000

    Fix when matrix is of different size

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,12 +4,14 @@ def print_matrix_integer(matrix=[[]]):
         return None
     else:
         end = 0
+        jump = 0
         for i in matrix:
             for j in i:
-                if end == 2:
+                if end == len(matrix[jump]) - 1:
                     print("{:d}".format(j), end="")
                     end = 0
                 else:
                     print("{:d} ".format(j), end="")
                     end += 1
+            jump += 1
             print("")
