55f07feda6633954154fccc6d2b389da89fc3975
mrdoomus <mrdoom.official@gmail.com>
2019-09-17 17:25:52 +0000

    fixed when list is empty, return the list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list == []:
-        return None
+        return my_list
     else:
         for i in range(len(my_list) - 1, -1, -1):
             print("{:d}".format(my_list[i]))
