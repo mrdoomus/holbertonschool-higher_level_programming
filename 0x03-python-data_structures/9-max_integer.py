b9de5ec6e20cf757dec59a5582816eb434c0f13a
mrdoomus <mrdoom.official@gmail.com>
2019-09-17 20:42:47 +0000

    Using i in list, not ranges

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
         return None
     else:
         maxnum = my_list[0]
-        for i in range(1, len(my_list)):
-            if my_list[i] > maxnum:
-                maxnum = my_list[i]
+        for i in my_list:
+            if i > maxnum:
+                maxnum = i
         return maxnum
