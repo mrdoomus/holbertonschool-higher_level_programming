5691d9802ebb81c7cb2d01762b4e4d65c9095cbb
mrdoomus <mrdoom.official@gmail.com>
2019-09-19 22:40:09 +0000

    Fix now only when IV, but when VX, XL, LC, CD, DM

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not type(roman_string) is str or roman_string is None:
+    if type(roman_string) is not str or roman_string is None:
         return 0
 
-    rev = roman_string[::-1]
     value = 0
     roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                  'C': 100, 'D': 500, 'M': 1000}
 
-    if rev[0] is not 'I':
-        roman_dic['I'] = -1
-        for i in roman_string:
-            value += roman_dic[i]
-    else:
-        for i in roman_string:
-            value += roman_dic[i]
+    if len(roman_string) == 1:
+        return roman_dic[roman_string]
+ 
+    for i in range(len(roman_string) - 1):
+        if roman_dic[roman_string[i]] < roman_dic[roman_string[i + 1]]:
+            roman_dic[roman_string[i]] = -(roman_dic[roman_string[i]])
+        
+    for j in roman_string:
+        value += roman_dic[j]
     return value
