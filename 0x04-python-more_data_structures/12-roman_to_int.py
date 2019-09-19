cb122b1d44c739daf822f7e23d9bf29287b0b3cd
mrdoomus <mrdoom.official@gmail.com>
2019-09-19 22:05:12 +0000

    Optimized code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,16 +2,17 @@
 def roman_to_int(roman_string):
     if not type(roman_string) is str or roman_string is None:
         return 0
+
     rev = roman_string[::-1]
     value = 0
+    roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+
     if rev[0] != 'I':
-        roman_dic_minus = {'I': -1, 'V': 5, 'X': 10, 'L': 50,
-                           'C': 100, 'D': 500, 'M': 1000}
+        roman_dic['I'] = -1
         for i in roman_string:
-            value += roman_dic_minus[i]
+            value += roman_dic[i]
     else:
-        roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                     'C': 100, 'D': 500, 'M': 1000}
         for i in roman_string:
             value += roman_dic[i]
     return value
