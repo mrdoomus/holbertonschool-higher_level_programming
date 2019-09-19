965d4e26dbd06cc25404479121273dc4ff34ab0b
mrdoomus <mrdoom.official@gmail.com>
2019-09-19 22:16:17 +0000

    Why does Hlberton school do this?

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    for key in a_dictionary:
-        if a_dictionary[key] is None:
-            return None
-    return max(a_dictionary)
+    if a_dictionary is None:
+        return None
+    else:
+        return max(a_dictionary)
+    #values = sorted(a_dictionary.values())
+    #print(values)
+    #res = values[-1]
+    #print(res)
+    #for key in a_dictionary:
+        #if res == a_dictionary[key]:
+            #return key
