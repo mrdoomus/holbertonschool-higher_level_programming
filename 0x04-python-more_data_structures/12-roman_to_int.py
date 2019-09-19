57598f266f3e94d7cfa6b394082e9bccab94e049
mrdoomus <mrdoom.official@gmail.com>
2019-09-19 22:06:33 +0000

    Quick fix, changed != to is not

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,7 @@ def roman_to_int(roman_string):
     roman_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                  'C': 100, 'D': 500, 'M': 1000}
 
-    if rev[0] != 'I':
+    if rev[0] is not 'I':
         roman_dic['I'] = -1
         for i in roman_string:
             value += roman_dic[i]
