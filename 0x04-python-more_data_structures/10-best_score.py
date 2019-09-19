84439170c72761d8cc564e844fafca69c0d49160
mrdoomus <mrdoom.official@gmail.com>
2019-09-19 21:02:29 +0000

    Removed some conditions, asked properly if a key has None value the nreturn None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    else:
-        for key in a_dictionary:
-            if a_dictionary[key] is 0:
-                return None
-        return max(a_dictionary)
+    for key in a_dictionary:
+        if a_dictionary[key] is None:
+            return None
+    return max(a_dictionary)
