5da89f4e838627dae56deb9ad500c3b2436c5e21
mrdoomus <mrdoom.official@gmail.com>
2019-09-19 18:10:48 +0000

    Removed if list existance control flows

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
         new_list = list(map(lambda n: replace if n == search else n, my_list))
         return new_list
-    else:
-        return None
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        uniq_set = set(my_list)
-        uniq_list = list(uniq_set)
-        num = uniq_list[0]
-        for i in range(1, len(uniq_list)):
-            num += uniq_list[i]
-        return num
-    else:
-        return None
+    uniq_set = set(my_list)
+    uniq_list = list(uniq_set)
+    num = uniq_list[0]
+    for i in range(1, len(uniq_list)):
+        num += uniq_list[i]
+    return num
