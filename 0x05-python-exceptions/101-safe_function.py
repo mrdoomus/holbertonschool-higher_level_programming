2e47373e1c17ffd210ea402bdaad11ddf1957fbe
mrdoomus <mrdoom.official@gmail.com>
2019-10-01 03:40:52 +0000

    Replaced pointer value to pinter

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,7 +4,7 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(args)
+        result = fct(*args)
     except Exception as err:
         print("Exception: {}".format(str(err)), file=sys.stderr)
         result = None
