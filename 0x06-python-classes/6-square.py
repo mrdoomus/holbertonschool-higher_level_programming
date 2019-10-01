677d12aec66d6013e648b9918ad71ee9790e07dc
mrdoomus <mrdoom.official@gmail.com>
2019-10-01 17:56:30 -0500

    Added new condition if tuple is a tuple and if its len is 2

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,8 +12,10 @@ class Square:
             raise ValueError('size must be >= 0')
         else:
             self.__size = size
-
-        if type(position[0]) is not int or type(position[1]) is not int:
+            
+        if type(position) is not tuple or len(position) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif type(position[0]) is not int or type(position[1]) is not int:
             raise TypeError('position must be a tuple of 2 positive integers')
         elif position[0] < 0 or position[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
@@ -55,7 +57,9 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value[0]) is not int or type(value[1]) is not int:
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError('position must be a tuple of 2 positive integers')
         elif value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
