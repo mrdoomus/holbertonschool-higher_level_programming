dfedc6acf1f01f702d3175106ee6ae92f8c243cf
Holberton <holberton@iMac-de-Holberton.local>
2019-10-22 16:34:59 -0500

    Uploaded 19th script

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,10 +28,10 @@ class Base:
     def save_to_file(cls, list_objs):
         """ Writes a json string to a json file """
         new_list = []
-        new_file = cls.__name__ + '.json'
+        file_name = cls.__name__ + '.json'
         if list_objs:
             new_list = [data.to_dictionary() for data in list_objs]
-        with open(new_file, 'w', encoding='utf-8') as f:
+        with open(file_name, 'w', encoding='utf-8') as f:
             f.write(Base.to_json_string(new_list))
 
     @staticmethod
@@ -40,12 +40,28 @@ class Base:
         if json_string:
             return loads(json_string)
         return []
-    
+
     @classmethod
     def create(cls, **dictionary):
+        """ Creates an instance of the class """
         if cls.__name__ == "Rectangle":
             dummy = cls(2, 5)
         elif cls.__name__ == "Square":
             dummy = cls(2)
         dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """ Loads a file of instances """
+        file_name = cls.__name__ + '.json'
+        with open(file_name, 'r', encoding='utf-8') as f:
+            new_list = []
+            file_dict = f.read()
+            if file_dict is None or len(file_dict) == 0:
+                return []
+            file_data = cls.from_json_string(file_dict)
+            for dictionary in file_data:
+                instance = cls.create(**dictionary)
+                new_list.append(instance)
+            return new_list
