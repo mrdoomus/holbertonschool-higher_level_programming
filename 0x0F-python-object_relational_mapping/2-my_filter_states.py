3344a084c606a7f12e836dfcf5cb0ebb5d433d99
mrdoomus <mrdoom.official@gmail.com>
2019-12-15 11:31:54 -0500

    master: Fixed 1st script by adding BINARY statement and 2nd script with format

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,8 +6,10 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', user=argv[1],
                          passwd=argv[2], db=argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s\
-    ORDER BY states.id ASC", (argv[4],))
+    cur.execute("SELECT * FROM states\
+        WHERE name\
+        LIKE BINARY '{}'\
+        ORDER BY states.id ASC".format(argv[4]))
     res = cur.fetchall()
     for data in res:
         print(data)
