61845d57a350cc189b2f9b81b1094d21d1629d9d
mrdoomus <mrdoom.official@gmail.com>
2020-01-06 15:16:36 -0500

    master: Fix removed curl flag -I and -L

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sLI "$1" -H "X-HolbertonSchool-User-Id: 98"
+curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
