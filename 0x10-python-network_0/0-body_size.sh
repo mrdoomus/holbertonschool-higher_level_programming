640da8899edc8c08dc1b9594ad62708109890bde
mrdoomus <mrdoom.official@gmail.com>
2020-01-07 10:59:10 -0500

    master: Fix changed whole script by using the -o flag

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | cut -f2 -d ' ' | tail -3 | head -1
+curl -so /dev/null "$1" -w '%{size_download}' 
