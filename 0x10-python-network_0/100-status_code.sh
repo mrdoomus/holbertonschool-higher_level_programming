feb38e3f60586177b81ccafee52df1cc4703b474
mrdoomus <mrdoom.official@gmail.com>
2020-01-06 19:36:50 -0500

    master: Fix Re-did script with requirements, deleted pipes

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL passed as an argument, and displays only the status code of the response
-curl -sI "$1" | head -1 | cut -d ' ' -f2
+curl -s -o /dev/null -I -w "%{http_code}" "$1"
