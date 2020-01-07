22863916e946e7c66ed0e0eebc892118277d1d70
mrdoomus <mrdoom.official@gmail.com>
2020-01-07 10:54:32 -0500

    master: Fix changed whole scrip, wc won't be working

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s "$1" | wc -c
+curl -sI "$1" | cut -f2 -d ' ' | tail -3 | head -1
