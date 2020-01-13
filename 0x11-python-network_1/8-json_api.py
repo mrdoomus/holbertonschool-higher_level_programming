e1c089565272f3b574df12a0eccae22edf5d241b
mrdoomus <mrdoom.official@gmail.com>
2020-01-13 16:30:32 -0500

    master: [Fix] Struggling with q variable

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,9 +6,11 @@ from sys import argv
 
 
 if __name__ == '__main__':
-    q = ""
+    q = ''
     if len(argv) == 2:
         q = argv[1]
+
+    if len(argv) == 2:
         req = post(
             'http://f944a8f69e43.19.hbtn-cod.io:5000/search_user', {'q': q})
         try:
