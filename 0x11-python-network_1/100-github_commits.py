b06eac1e98b56893069ca00099de1c57cabb1295
mrdoomus <mrdoom.official@gmail.com>
2020-01-13 19:17:40 -0500

    master: [Upload] script that lists last 10 commits from rails repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""list 10 commits from rails repo"""
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    full_url = 'https://api.github.com/repos/'
+    + argv[2] + '/' + argv[1] + '/' + 'commits'
+    url = get(full_url)
+    for key in url.json()[:10]:
+        print("{}: {}".format(key.get('sha'),
+              key.get('commit').get('author').get('name')))
