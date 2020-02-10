7c2549f3f601d35ccc088c335d52589a3e9c98db
mrdoomus <mrdoom.official@gmail.com>
2020-02-10 17:41:34 -0500

    master: [Fix] Done it without reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return (list.reverse());
+  let j = 0;
+  const finalList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    finalList[j] = list[i];
+    j++;
+  }
+  return (finalList);
 };
